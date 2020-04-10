6f6759570d634affa1856a8ecccc71f1220b322b
CobaltoIV <62718484+CobaltoIV@users.noreply.github.com>
2020-04-10 19:14:13 +0100

    Create part3vowel.m

---
+++ Lab 2/part3vowel.m
@@ -0,0 +1,110 @@
+%% Computing the average f0 for the birthdate speech signal
+[x, Fs] = audioread('vowels_87005.wav');
+
+dummy=load('ideal_e.mat');
+
+ideal_e = dummy.e;
+
+% Defining lengths for the window and the loop
+total_time = length(x);
+intervalo = 0.01*Fs;
+tamanho = 0.02*Fs;
+nwindows = total_time/intervalo - 1;
+pitch =  146.0082;
+p=16;
+f0min = 60;
+f0max = 200;
+
+%Defining vectors to store the data
+ak = lpcauto(x,p,[intervalo, tamanho, 0]);
+F0=[];
+e = [];
+%Threshold for the unvoiced sounds
+threshold = 0.3;
+
+% Main loop
+for i=1:(nwindows-1)
+    % Getting the signal and it's correlation
+    y = x((i-1)*intervalo + 1 : (i+1)*intervalo);
+    r = xcorr(y);
+
+    % Getting the energy of the signal and normalizing it
+    m = max(r);
+    norm = r/m;
+    
+    % Removing unvoiced sounds
+    for j=1:length(norm)
+        if(norm(j) < threshold)
+            norm(j) = 0;
+        end
+    end    
+    
+    % Computing f0
+    [f0, m2] =  calcf0(norm,tamanho,Fs,f0max, f0min);
+   
+    % Eliminating the frequency for sounds with really low energy
+    if (m <= 10^-2)
+        f0 = 0;
+    end
+    % Making f0=0 where the max is an unvoiced sound
+    if (m2 == 0)
+        f0 = 0;
+    end
+    F0(i) = f0;
+    if f0==0     
+     % e((i-1)*intervalo + 1 : (i+1)*intervalo,1) = randn(1, length(y));
+       e((i-1)*intervalo + 1 : (i+1)*intervalo,1) = randn(1, length(y))*0.001;
+       prev_zeros=0;
+    else
+      z = zeros(size(y));
+      
+      imp_space = round(Fs/pitch);
+      
+      start=imp_space-prev_zeros;
+      
+      if start<1
+         z(1:imp_space:end) =  0.1;
+      else
+         z(start:imp_space:end) =  0.1;
+      end
+      
+      e((i-1)*intervalo + 1 : (i+1)*intervalo,1) = z;
+      %Count zeros
+      k=find(z);
+      prev_zeros=length(z)-k(length(k));
+    end
+    
+   
+    
+end
+noise = randn(intervalo/2,1)*0.001;
+e = vertcat(noise,e);
+e = vertcat(e,noise);
+
+
+
+%% Generate signal based on e
+
+Zi=[];
+xr=[];
+
+  for j=1:(nwindows)
+
+    init = (j*intervalo-intervalo/2)+1;
+    if j==1 % if it's the first window
+        init=1;
+    end
+    final = j*intervalo+intervalo/2;
+    if j==nwindows % if it's the last window
+        final=length(x);
+    end
+
+    segment = e(init:final);
+    [y,Zi] = filter(1,ak(j,:),segment,Zi);
+
+
+    xr = vertcat(xr,y);
+
+  end
+ 
+ audiowrite('vowels_87005_voc.wav',xr,Fs);
